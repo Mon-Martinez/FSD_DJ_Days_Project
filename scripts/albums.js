c8a2c484424c2dbc12c84e6c95601dfc537534dd
Mon-Martinez <126629463+Mon-Martinez@users.noreply.github.com>
2023-03-07 18:09:11 -0600

    Create albums array and add 10 objects

--- scripts/albums.js
+++ scripts/albums.js
@@ -6,3 +6,65 @@
   Optionally, you can also add an "artwork" property with an image of the album cover.
 */
 
+let albums = [
+  {
+    name: "1",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "2",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "3",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "4",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "5",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "6",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "7",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "8",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "9",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  },
+  {
+    name: "10",
+    artist: "",
+    genre: "",
+    artwork: "img/demopic.jpg"
+  }
+]
