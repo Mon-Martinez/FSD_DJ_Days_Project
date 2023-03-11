dfe85ef2731d1753ed62f3a50734ee2419f3774b
Mon-Martinez <126629463+Mon-Martinez@users.noreply.github.com>
2023-03-11 11:09:42 -0600

    Added boolean values, conditional statement, and additional html to show top three albums on the DOM

--- scripts/albumListings.js
+++ scripts/albumListings.js
@@ -7,19 +7,29 @@
 
 //The message variables have also been included
 let html = '';
+let topThreeMessage ="This is one of my Top Three albums!";
 
 for (let i = 0; i < albums.length; i++){
     html += `
         <h2>${albums[i].name}</h2>
         <h3>${albums[i].artist}</h3>
         <p>Genre: ${albums[i].genre}</p>
-        <img src="${albums[i].artwork}" alt="Album art for ${albums[i].name}">
+    `;
+    let topThree = albums[i].topThree;
+    if (topThree) {
+        html += `
+        <p>${topThreeMessage}</p>
+        `;
+    };
+    html += `
+    <img src="${albums[i].artwork}" alt="Album art for ${albums[i].name}">
     `;
 }
 
 let moreAlbumsMessage = "You need more albums!";
 let playMusicMessage = "Let's start playing music!";
 
+// All aspects of the if else statment were produced during the in class demo
 if (albums.length < 10){
     document.querySelector('header').insertAdjacentHTML('beforeend', `<h4>${moreAlbumsMessage}</h4>`);
     console.log ('There are less than 10 albums');
@@ -28,14 +38,4 @@ if (albums.length < 10){
     console.log ('There are 10 albums or more');
 }
 
-// Trying to make a boolean conditional statement
-let topThree = true;
-if (topThree = true) {
-    document.querySelector('main').insertAdjacentHTML('beforeend', <p>This is one of my Top Three Albums!</p>);
-    console.log ('I am showing my Top Three Albums');
-}
-
 document.querySelector('main').insertAdjacentHTML('beforeend', html);
-
-// Project is complete
-// All aspects of the if else statment were produced during the in class demo
--- scripts/albums.js
+++ scripts/albums.js
@@ -8,61 +8,70 @@
 
 let albums = [
   {
+    topThree: true,
     name: "RENAISSANCE",
     artist: "Beyonce",
     genre: "Dance Pop",
     artwork: "imgs/1.png"
   },
   {
+    topThree: false,
     name: "Special",
     artist: "Lizzo",
     genre: "Funk-Pop Hip Hop",
     artwork: "imgs/2.png"
   },
   {
+    topThree: false,
     name: "Blonde",
     artist: "Frank Ocean",
     genre: "Avant-gard soul",
     artwork: "imgs/3.jpeg"
   },
   {
+    topThree: true,
     name: "30",
     artist: "Adele",
     genre: "Jazz Pop",
     artwork: "imgs/4.png"
-    topThree: "True"
   },
   {
+    topThree: false,
     name: "God Save the Animals",
     artist: "Alex G",
     genre: "Alternative Indie",
     artwork: "imgs/5.png"
   },
   {
+    topThree: false,
     name: "The Dutchess",
     artist: "Fergie",
     genre: "Pop R&B",
     artwork: "imgs/6.png"
   },
   {
+    topThree: false,
     name: "Beyondless",
     artist: "Iceage",
     genre: "Post-Punk",
     artwork: "imgs/7.jpeg"
   },
   {
+    topThree: false,
     name: "Interior Person",
     artist: "Samantha Sidley",
     genre: "Jazz",
     artwork: "imgs/8.jpeg"
   },
   {
+    topThree: false,
     name: "Tell Me How You Really Feel",
     artist: "Courtney Barnett",
     genre: "Alternative Indie",
     artwork: "imgs/9.jpeg"
   },
   {
+    topThree: true,
     name: "Con Todo El Mundo",
     artist: "Khruangbin",
     genre: "Psychedelic Surf Rock",
